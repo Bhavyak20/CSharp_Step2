33878102dc8881bd56e6674451cb6e52343f1e48
Bhavyak20 <bhavyak.fullstack@gmail.com>
2025-07-08 00:07:30 +0530

    Add project files.

---
+++ CSharp_Step2/Inheritance/ClassInheritance.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Security.Policy;
+
+namespace Family
+
+{
+    /// <summary>
+    /// This code is about inheritance from parent class to child class.
+    /// here we didn't used paramertrs in the code.
+    /// </summary>
+    class Father
+
+    {
+        public string fathername { get; set; }
+        public string mothername = "Alice";
+        public Father()
+        {
+            fathername = "JOE";//here assigned father name in constructor
+            Console.WriteLine("**Father class constructor**");
+        }
+        public void getMessage()
+        {
+            Console.WriteLine($"Name of the father is {fathername}");
+        }
+
+        class Child : Father//inherited father class
+        {
+            public string childname = "Ken";
+            public Child()
+            {
+                childname = "Vin";//updated value of childname
+                fathername = "Dev";//we can also updat fathername here in child class because of inheritance. 
+                Console.WriteLine("Child class constructor");
+            }
+            public void Message()
+            {
+                Console.WriteLine($"Name of the child is {childname}");
+            }
+        }
+
+        class MainProgram
+        {
+            static void Main()
+            {
+                // Father father = new Father();
+                //father.getMessage();
+
+                Child child = new Child();
+                child.getMessage();
+                child.Message();
+
+
+            }
+        }
+
+
+    }
+}
+
---
+++ CSharp_Step2/Inheritance/Inheritance_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Company
+
+{
+    class Company
+    {
+        public string CompanyName = "TCS";
+        public string CompanyLocation = "Chennai";
+        public int Companycode;
+        public Company(int companycode)
+        {
+            Companycode = companycode;
+            Console.WriteLine("This is company Class constructor");
+        }
+        public void Message()
+        {
+            Console.WriteLine($"{CompanyName} is located in {CompanyLocation}.");
+        }
+    }
+
+    class Employe : Company
+    {
+        public string EmployeName = "Raj";
+        public Employe(int companycode) : base(companycode)
+        {
+            Console.WriteLine("this is Employe class Constructor");
+        }
+
+        public void EmployeMessage()
+        {
+            Console.WriteLine($"{EmployeName} is working in {CompanyName} at {CompanyLocation} and code is {Companycode}");
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Employe emp = new Employe(2901);
+                emp.Message();
+                emp.EmployeMessage();
+            }
+        }
+    }
+}
